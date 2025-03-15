0a9006cb6fa146fd3a709c5db68cb66b05af7816
TenshiLenovo <zakan.6000@gmail.com>
2025-03-15 15:29:47 +0800

    add wires

---
+++ wires/main.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from skimage.measure import label
+from skimage.morphology import (binary_closing, binary_opening, 
+                                binary_dilation, binary_erosion)
+
+for i in range(1, 7):
+    data = np.load(f'wires{i}npy.txt')
+    labeled = label(data)
+    print(f'Изначальное кол-во проводов: {np.max(labeled)}')
+    plt.imshow(labeled)
+    plt.show()
+
+    count_ = 0
+    for j in range(1, np.max(labeled) + 1):
+        result = label(binary_erosion(labeled==j, np.ones(3).reshape(3, 1)))
+        c = np.max(result)
+        match c:
+            case 0: print(f'{j} провод уничтожен')
+            case 1: print(f'{j} провод цел')
+            case _: print(f'{j} провод разрезан на {c} частей')
