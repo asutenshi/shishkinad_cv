c152314694aea92088f936b068d7bd7a6d8ef959
TenshiLenovo <zakan.6000@gmail.com>
2025-05-17 11:02:20 +0800

    4k points trex

--- trex/main.py
+++ trex/main.py
@@ -6,17 +6,7 @@ import threading
 import time
 import webbrowser
 
-BASE_SPEED = 6          # юн./кадр
-ACC_PER_SEC = 0.001 * 60  # 0.06 юн./с
-MAX_SPEED  = 13
-PX_PER_UNIT = 70
-
-BASE_DISTANCE = 150 # 466
-
-# def jump_and_duck(duck_time=0.2):
-#     pyautogui.press('space')
-#     pyautogui.keyDown('down')
-#     threading.Timer(duck_time, pyautogui.keyUp, args=('down',)).start()
+BASE_DISTANCE = 390 # 390
 
 def screen_capture(start_event):
 # def screen_capture():
@@ -24,20 +14,26 @@ def screen_capture(start_event):
 
     start_time = time.perf_counter()
     coef = 1.0
+    speed = 3.6
 
     with mss.mss() as sct:
         # monitor = {"top" : 165, "left" : 681, "width" : 615, "height" : 117}
         monitor = {"top" : 410, "left" : 170, "width" : 1250, "height" : 233}
+        # monitor = {"top" : 400, "left" : 57, "width" : 865, "height" : 180}
         cv2.namedWindow("Screen Capture", cv2.WINDOW_NORMAL)
 
         while True:
             screenshot = sct.grab(monitor) 
             frame = np.array(screenshot)
 
-            elapsed = time.perf_counter() - start_time      # секунды
-            speed_units = min(BASE_SPEED + ACC_PER_SEC * elapsed, MAX_SPEED)
-            coef = speed_units * PX_PER_UNIT
-            jump_threshold = BASE_DISTANCE + coef
+            # curr_time = time.perf_counter()
+            # print(f'FPS = {1 / (curr_time - prev_time):.1f}')
+            # prev_time = curr_time
+
+            # plt.figure()
+            # plt.imshow(frame)
+            # plt.show()
+            # break
 
             gray = cv2.cvtColor(frame, cv2.COLOR_BGRA2GRAY)
             _, binary = cv2.threshold(gray, 83, 255, cv2.THRESH_BINARY_INV)
@@ -45,14 +41,24 @@ def screen_capture(start_event):
             contours, _ = cv2.findContours(binary, cv2.RETR_EXTERNAL,
                                                   cv2.CHAIN_APPROX_SIMPLE)
 
+            timer = time.perf_counter() - start_time
+            if timer < 116.7:
+                coef = timer * speed
+            # print(timer)
+
+            jump_threshold = BASE_DISTANCE + coef
+            # print(jump_threshold)
             for i, contour in enumerate(contours):
                 (x, y, w, h) = cv2.boundingRect(contour)
                 if w*h > 600:
                     if y + h > 178  and 40+w < x+w < jump_threshold:
                         pyautogui.press('space')
-                        pyautogui.press('down')
+                    # if i == 1:
+                    #     cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+                    # else:
+                    #     cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
-            cv2.imshow("Screen Capture", frame)
+            # cv2.imshow("Screen Capture", frame)
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
 
